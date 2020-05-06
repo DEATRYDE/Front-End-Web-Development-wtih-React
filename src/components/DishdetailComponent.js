46570a652317d0835efa902f0d96e346467d70aa
Rahul Dogra <rahuldogra1998@gmail.com>
2020-05-06 15:31:11 +0530

    React Animation Componenets

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,31 +19,36 @@ import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function RenderComments({ comments, postComment, dishId }) {
   // console.log(comments)
   if (comments != null) {
-    let list = comments.map((comments) => {
+    let commentlist = comments.map((comments) => {
       return (
-        <li key={comments.id}>
-          <div>
-            <p>{comments.comment}</p>
-            <p>
-              --{comments.author},
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comments.date)))}
-            </p>
-          </div>
-        </li>
+        <Fade in>
+          <li key={comments.id}>
+            <div>
+              <p>{comments.comment}</p>
+              <p>
+                --{comments.author},
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }).format(new Date(Date.parse(comments.date)))}
+              </p>
+            </div>
+          </li>
+        </Fade>
       );
     });
     return (
       <div className="col-12 col-md m-1">
         <h4>Comments</h4>
-        <ul className="list-unstyled">{list}</ul>
+        <ul className="list-unstyled">
+          <Stagger in>{commentlist}</Stagger>
+        </ul>
         <CommentForm dishId={dishId} postComment={postComment} />
       </div>
     );
@@ -57,13 +62,20 @@ function RenderDish({ dish }) {
   if (dish != null) {
     return (
       <div className="col-12 col-md m-1">
-        <Card>
-          <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
+        <FadeTransform
+          in
+          transformProps={{
+            exitTransform: "scale(0.5) translateY(-50%)",
+          }}
+        >
+          <Card>
+            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </FadeTransform>
       </div>
     );
   } else {
